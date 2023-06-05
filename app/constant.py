38c0cd72de850422b10fbc909aec3bf2ebc790ea
Akash Garg <akash.garg@siemens.com>
2023-06-05 16:21:42 +0530

    fix code

--- app/constant.py
+++ app/constant.py
@@ -1,9 +1,12 @@
-indian_index = {"Nifty 50":"^NSEI", "Bank Nifty": "^NSEBANK", "Sensex": "^BSESN", "India VIX": "^INDIAVIX"}
-global_indicators = { "s&p 500": "^GSPC",  
-                     "nasdaq":"^IXIC", 
-                     "don_jones": "^DJI",  					
-                    "Dollar": "INR=X",
-                    "Euro": "EURINR=X" 
-                    }
+indian_index = {"Nifty 50": "^NSEI", "Bank Nifty": "^NSEBANK",
+                "Sensex": "^BSESN", "India VIX": "^INDIAVIX"}
+
+global_indicators = {"S&P 500": "^GSPC",
+                     "Nasdaq": "^IXIC",
+                     "Dow Jones": "^DJI",
+                     "Dollar": "INR=X",
+                     "Euro": "EURINR=X"
+                     }
+
 crypto_currency = {"Bitcoin": "BTC-INR",
-                    "Ethereum": "ETH-INR"}
+                   "Ethereum": "ETH-INR"}
